7798d9e6e3b82acb1238dfd2e71ed2137d203cc6
Merajujjaman Meraj <asmmerajujjaman@gmail.com>
2025-06-26 21:24:52 +0600

    ready to deploy

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
-const API_URL ="http://localhost:5000/api";
-const SOCKET_URL ="http://localhost:5000";
+const API_URL ="https://meraj-chat-app-server.onrender.com/api";
+const SOCKET_URL ="https://meraj-chat-app-server.onrender.com";
+// const API_URL ="http://localhost:5000/api";
+// const SOCKET_URL ="http://localhost:5000";
 
 export { API_URL, SOCKET_URL };
